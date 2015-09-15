32c17faf6e9b2de6ec7600c3d36d209d5aa3766b
IanEisenberg <IanEisenberg90@gmail.com>
2015-09-14 18:57:52 -0700

    added task to launchScan script

--- finger_tapping/finger_tapping_script_psychopylaunchScan.py
+++ finger_tapping/finger_tapping_script_psychopylaunchScan.py
@@ -2,10 +2,16 @@ from psychopy import visual, event, core, gui
 from psychopy.hardware.emulator import launchScan
 from psychopy.sound import Sound
 
+#task settings
+alternate_time = 8 #number of TRs for each block
+n_blocks = 24  #12 on, 12 off
+task_duration = n_blocks * alternate_time #in TRs, 6 minute scan sessions with 2 second TR
+window_dims = [800,600]                              
+
 # settings for launchScan:
 MR_settings = { 
     'TR': 2.000, # duration (sec) per volume
-    'volumes': 5, # number of whole-brain 3D volumes / frames
+    'volumes': task_duration, # number of whole-brain 3D volumes / frames
     'sync': '5', # character to use as the sync timing event; assumed to come at start of a volume
     'skip': 0, # number of volumes lacking a sync pulse at start of scan (for T1 stabilization)
     'sound': True, # in test mode only, play a tone as a reminder of scanner noise
@@ -13,18 +19,17 @@ MR_settings = {
 mode = 'Test' #'Test' or 'Scan' or 'None' to choose at startup
 globalClock = core.Clock()
 
-
 infoDlg = gui.DlgFromDict(MR_settings, title='fMRI parameters', order=['TR','volumes'])
 if not infoDlg.OK: core.quit()
 
-window_dims = [800,600]
+#Setup task elements
 win = visual.Window(window_dims, allowGUI=False, fullscr= False, 
-                                 monitor='testMonitor', units='deg')                                  
+                                 monitor='testMonitor', units='deg')    
 fixation_cross=visual.TextStim(win, text='+',font='BiauKai',
                                 height=2,color=[1,1,1], colorSpace=u'rgb',
                                 opacity=1,depth=0.0,
                                 alignHoriz='center',wrapWidth=50)
-
+counter = visual.TextStim(win, height=.5, pos=(0,-4), color=win.rgb+0.5)
 
 # summary of run timing, for each key press:
 output = u'vol    onset key\n'
@@ -32,7 +37,6 @@ for i in range(-1 * MR_settings['skip'], 0):
     output += u'%d prescan skip (no sync)\n' % i
 
 key_code = MR_settings['sync']
-counter = visual.TextStim(win, height=1, pos=(0,0), color=win.rgb+0.5)
 output += u"  0    0.000 %s  [Start of scanning run, vol 0]\n" % key_code
 sync_now = False
 
@@ -63,6 +67,10 @@ while globalClock.getTime() < duration:
         counter.setText(u"Elapsed volumes: %d\n%.3f seconds" % (vol, onset))
         output += u"%3d  %7.3f %s\n" % (vol, onset, sync_now)
         counter.draw()
+        #alternate fixation
+        on = vol%(alternate_time*2)>alternate_time
+        if on:
+            fixation_cross.draw()
         win.flip()
         vol += 1
         sync_now = False
